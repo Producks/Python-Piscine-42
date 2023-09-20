850aa53587fe168549c166117af94e9df8b42b44
dave <davetrap.demers@gmail.com>
2023-09-20 14:52:36 -0400

    Push for home

--- module_00/ex05/building.py
+++ module_00/ex05/building.py
@@ -1,5 +1,5 @@
 import sys
-from string import punctuation
+
 
 def main(argv):
     '''Main function bozo'''
@@ -11,12 +11,27 @@ def main(argv):
     else:
         print("AssetionError too many arguments")
         return 1
-    print(sum(1 for c in message if c.isupper()), "upper letters")
-    print(sum(1 for c in message if c.islower()), "lower letters")
-    print(sum(1 for c in message if c in punctuation), "punctuation marks")
-    print(sum(1 for c in message if c.isspace()), "spaces")
-    print(sum(1 for c in message if c.isdigit()), "digits")
+    total = upper = lower = bozo = spaces = digits = 0
+    for i in message:
+        total += 1
+        if i.isupper() is True:
+            upper += 1
+        elif i.islower() is True:
+            lower += 1
+        elif i in ["!", "\"", "'", ",", "-", ".", ":", ";", "\n"]:
+            bozo += 1
+        elif i.isspace() is True:
+            spaces += 1
+        elif i.isdigit() is True:
+            digits += 1
+    print(f"The text contains {total} characters:")
+    print(f"{upper} upper letters")
+    print(f"{lower} lower letters")
+    print(f"{bozo} punctuation marks")
+    print(f"{spaces} spaces")
+    print(f"{digits} digits")
     return 0
+
 
 if __name__ == "__main__":
     main(sys.argv)
--- module_00/ex06/filterstring.py
+++ module_00/ex06/filterstring.py
@@ -1,1 +1,18 @@
 import sys
+
+def check_even(number):
+    if number % 2 == 0:
+          return True  
+    return False
+
+def ft_filter(func: any, object: any):
+    '''filter(function or None, iterable) --> filter object
+
+Return an iterator yielding those items of iterable for which function(item)
+is true. If function is None, return the items that are true.'''
+    return print(filter.__doc__)
+
+if __name__ == "__main__":
+    ft_list = ["BOZO", ".", "com"]
+    ft_filter()
+    print(filter.__doc__)
